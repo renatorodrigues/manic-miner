509420313d6367f3edddb13422a703aca409237e
Pedro Borges <highvolt16@gmail.com>
2013-04-07 00:24:42 +0100

    font cache function

--- modules/myModule/main.cs
+++ modules/myModule/main.cs
@@ -1,6 +1,13 @@
 //walls collision group = 1
 
-
+function populateFonts()
+{      
+      %font = "Arial";      
+      %sizes = "18 24 32 36";      
+      for (%i = 0; %i < getWordCount(%sizes); %i++)            
+            populateFontCacheRange(%font, getWord(%sizes, %i), 32, 126);      
+      writeFontCache();
+}
 
 
 function myModule::create( %this )
