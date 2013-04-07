fcefc1767772e089858316c2b0c7a359e64a5521
Bruno Maia <queimadus@gmail.com>
2013-04-07 20:21:10 +0100

    ignored backup and osx related files added bottom tile sprite repeat added wall image and sprite repeat added floor image

--- modules/myModule/scripts/normal_floor.cs
+++ modules/myModule/scripts/normal_floor.cs
@@ -1,27 +1,38 @@
-
-
 function addNormalFloor(%x,%y,%w,%h){
 		
-	myModule.floor[myModule.nfloor]=new Sprite();
-	myModule.floor[myModule.nfloor].setBodyType( static );
+  myModule.floor[myModule.nfloor]=new Scroller();//new Sprite();
+  myModule.floor[myModule.nfloor].setBodyType( static );
 
 
-    myModule.floor[myModule.nfloor].Position = %x SPC %y;
+  myModule.floor[myModule.nfloor].Position = %x SPC %y;
 
-    
-    myModule.floor[myModule.nfloor].Size = %w SPC %h;
-   myModule.floor[myModule.nfloor].Friction=0;
+
+  myModule.floor[myModule.nfloor].Size = %w SPC %h;
+  myModule.floor[myModule.nfloor].Friction=0;
   myModule.floor[myModule.nfloor].SceneLayer = 5;
-  myModule.floor[myModule.nfloor].SceneGroup = $NORMAL_TILE ;
-   
-   myModule.floor[myModule.nfloor].Image = "myModule:Tile";
-    myModule.floor[myModule.nfloor].UseBackgroundColor = true;
-    myModule.floor[myModule.nfloor].createEdgeCollisionShape( -%w/2, -%h/2, -%w/2, %h/2 );
-   myModule.floor[myModule.nfloor].createEdgeCollisionShape( %w/2, -%h/2, %w/2, %h/2);
-   myModule.floor[myModule.nfloor].createEdgeCollisionShape( -%w/2, %h/2, %w/2, %h/2 );
-    myModule.floor[myModule.nfloor].createEdgeCollisionShape( -%w/2, -%h/2, %w/2, -%h/2 );   
-    myScene.add( myModule.floor[myModule.nfloor]);  
+  myModule.floor[myModule.nfloor].SceneGroup = $NORMAL_TILE;
 
-    myModule.nfloor++;
+  if(%w>=%h){
+    myModule.floor[myModule.nfloor].Image = "myModule:Tile";
+    myModule.floor[myModule.nfloor].setRepeatX(%w/2);
+    
+  }else{
+    myModule.floor[myModule.nfloor].Image = "myModule:Wall";
+    myModule.floor[myModule.nfloor].setRepeatY(%h/3);
+     
+  }
+
+  myModule.floor[myModule.nfloor].UseBackgroundColor = true;
+  myModule.floor[myModule.nfloor].createEdgeCollisionShape( -%w/2, -%h/2, -%w/2, %h/2 );
+  myModule.floor[myModule.nfloor].createEdgeCollisionShape( %w/2, -%h/2, %w/2, %h/2);
+  myModule.floor[myModule.nfloor].createEdgeCollisionShape( -%w/2, %h/2, %w/2, %h/2 );
+  myModule.floor[myModule.nfloor].createEdgeCollisionShape( -%w/2, -%h/2, %w/2, -%h/2 );
+  myModule.floor[myModule.nfloor].setSize(%w,%h);
+ 
+  
+
+  myScene.add( myModule.floor[myModule.nfloor]);  
+
+  myModule.nfloor++;
 
 }
